48b4ca4472502832dc0b22276b41ff19c1a29dcc
pinpoint <picpoint@yandex.ru>
2021-04-20 20:49:09 +0300

    work with layout header page

--- src/index.php
+++ src/index.php
@@ -6,13 +6,35 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Антиквар - сайт по продаже и покупке антиквариата</title>
+    <link rel="stylesheet" href="/../public/css/font-awesome.min.css">
     <link rel="stylesheet" href="/../public/css/style.css">
 </head>
 <body>
 
     <section class="indx">
         <div class="indx__wrp">
+            <div class="indx__hdr">
+                <div class="indx__hdrcontent">
+                    <div class="indx__hdrcontacts">
+                        <div class="indx__hdrcontactstel">
+                            <i class="fa fa-phone-square" aria-hidden="true"></i>
+                            <span>+7(968)-535-99-29</span>
+                        </div>
+                        <div class="indx__hdrcontacstmail">
+                            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                            <span>info@antikvar.ru</span>
+                        </div>
+                    </div>
+                    <div class="indx__hdrlogo">
+                        <img src="/../public/pict/logo_bw2.png" alt="logo">
+                    </div>
+                    <div class="indx__hdrauth">
+                        <div class="indx__hdrauthblock">
 
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
