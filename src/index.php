4f5e213ad0a8135e6dbe4623c2954156512d8f2e
rmtar <picpoint@yandex.ru>
2021-04-20 22:47:40 +0300

    work with layout menu

--- src/index.php
+++ src/index.php
@@ -13,6 +13,7 @@
 
     <section class="indx">
         <div class="indx__wrp">
+
             <div class="indx__hdr">
                 <div class="indx__hdrcontent">
                     <div class="indx__hdrcontacts">
@@ -29,12 +30,34 @@
                         <img src="/../public/pict/logo_bw2.png" alt="logo">
                     </div>
                     <div class="indx__hdrauth">
-                        <div class="indx__hdrauthblock">
-
+                        <div class="indx__hdrauthreg">
+                            <i class="fa fa-address-card" aria-hidden="true"></i>
+                            <a href="#"><span>Регистрация</span></a>
+                        </div>
+                        <div class="indx__hdrauthlogin">
+                            <i class="fa fa-user-circle" aria-hidden="true"></i>
+                            <a href="#"><span>Авторизация</span></a>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="indx__menu">
+                <div class="indx__menucontent">
+                    <div class="indx__menucontentlinks">
+                        <a href="#">Ссылка 1</a>
+                        <a href="#">Ссылка 2</a>
+                        <a href="#">Ссылка 3</a>
+                        <a href="#">Ссылка 4</a>
+                        <a href="#">Ссылка 5</a>
+                    </div>
+                    <div class="indx__menucontentfind">
+                        <input type="search" placeholder="поиск по сайту">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </section>
 
