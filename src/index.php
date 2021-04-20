1cc9f75218fc1d097f9dab775fcf19e3ada75710
picpoint <picpoint@yandex.ru>
2021-04-20 08:56:18 +0300

    start work with index layout

--- src/index.php
+++ src/index.php
@@ -6,16 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Антиквар - сайт по продаже и покупке антиквариата</title>
+    <link rel="stylesheet" href="/../public/css/style.css">
 </head>
 <body>
 
-    <h3>Антиквар(сайт антиквариата)</h3>
-    <br>
-    <br>
+    <section class="indx">
+        <div class="indx__wrp">
 
-    <?php
-        var_dump($_SERVER['REQUEST_URI']);
-    ?>
+        </div>
+    </section>
+
+
 
 </body>
 </html>
